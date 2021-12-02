936b20b30c5f0699e68c9a0d07c42ae0a67542e2
Duuk Niemantsverdriet <D.P.Niemantsverdriet@student.tudelft.nl>
2021-12-02 06:20:21 +0100

    Clean day 1 code

--- days/day01.py
+++ days/day01.py
@@ -1,17 +1,17 @@
 from lib import read_input
 
 
-def solve(input):
+def solve(data):
     # return part_one(input)
-    return part_two(input)
+    return part_two(data)
 
 
-def part_one(input):
-    return sum([1 if input[i] > input[i - 1] else 0 for i in range(1, len(input))])
+def part_one(data):
+    return sum([1 if data[i] > data[i - 1] else 0 for i in range(1, len(data))])
 
 
-def part_two(input):
-    return sum([1 if input[i] > input[i - 3] else 0 for i in range(3, len(input))])
+def part_two(data):
+    return sum([1 if data[i] > data[i - 3] else 0 for i in range(3, len(data))])
 
 
 if __name__ == "__main__":
