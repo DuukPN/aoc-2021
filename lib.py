fcc1d4c664584bf2594047babb81589481f7e108
Duuk Niemantsverdriet <D.P.Niemantsverdriet@student.tudelft.nl>
2021-12-02 06:17:59 +0100

    Add small inputs for days 1 and 2

--- lib.py
+++ lib.py
@@ -1,8 +1,11 @@
 import os
 
 
-def read_input(day):
-    path = f"{os.path.dirname(os.getcwd())}/input/day{str(day).zfill(2)}.txt"
+def read_input(day, path=None):
+    if path:
+        path = f"{os.path.dirname(os.getcwd())}/input/day{str(day).zfill(2)}_{path}.txt"
+    else:
+        path = f"{os.path.dirname(os.getcwd())}/input/day{str(day).zfill(2)}.txt"
     f = open(path)
     data = []
     for line in f:
