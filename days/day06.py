0192f796d57b438ff626768c5600056c9a0b9990
Duuk Niemantsverdriet <D.P.Niemantsverdriet@student.tudelft.nl>
2021-12-06 06:58:43 +0100

    Add day 6

---
+++ days/day06.py
@@ -0,0 +1,56 @@
+from lib import load_input
+from collections import defaultdict
+
+day = 6
+
+
+def solve(data, part=2):
+    lines = [int(x) for x in data.split(",")]
+    if part == 1:
+        return part_one(lines)
+    elif part == 2:
+        return part_two(lines)
+
+
+def part_one(data):
+    return population(data, 80)
+
+
+def part_two(data):
+    return population(data, 256)
+
+
+def population(data, days):
+    fish = defaultdict(int)
+    for x in data:
+        fish[x] += 1
+
+    for i in range(days):
+        hatched = fish[0]
+        for j in range(8):
+            fish[j] = fish[j + 1]
+        fish[6] += hatched
+        fish[8] = hatched
+
+    total = 0
+    for i in range(9):
+        total += fish[i]
+
+    return total
+
+
+def naive_population(data, days):
+    my_data = data
+    for i in range(days):
+        for x in range(len(my_data)):
+            if my_data[x] == 0:
+                my_data[x] = 7
+                my_data.append(8)
+            my_data[x] -= 1
+
+    return len(my_data)
+
+
+if __name__ == "__main__":
+    print(solve(load_input(day), 1))
+    print(solve(load_input(day)))
