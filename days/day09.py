fbbd404abc1fc3fed0f57e76ef8f14c45caebe20
Duuk Niemantsverdriet <D.P.Niemantsverdriet@student.tudelft.nl>
2021-12-09 12:26:50 +0100

    Improve day 9

--- days/day09.py
+++ days/day09.py
@@ -12,6 +12,47 @@ def solve(data, part=2):
 
 
 def part_one(data):
+    low_points = get_low_points(data)
+    total = 0
+    for i, j in low_points:
+        total += int(data[i][j]) + 1
+
+    return total
+
+
+def part_two(data):
+    taken = [[False] * len(data[0]) for _ in range(len(data))]
+    basins = []
+    for basin, p in enumerate(get_low_points(data)):
+        neighbors = [p]
+        basins.append(0)
+        while neighbors:
+            i, j = neighbors.pop()
+
+            if taken[i][j]:
+                continue
+
+            taken[i][j] = True
+
+            if data[i][j] == "9":
+                continue
+
+            basins[basin] += 1
+
+            if i != 0 and not taken[i - 1][j]:
+                neighbors.append((i-1, j))
+            if i != len(data) - 1 and not taken[i + 1][j]:
+                neighbors.append((i+1, j))
+            if j != 0 and not taken[i][j - 1]:
+                neighbors.append((i, j-1))
+            if j != len(data[i]) - 1 and not taken[i][j + 1]:
+                neighbors.append((i, j+1))
+
+    basins.sort(reverse=True)
+    return basins[0] * basins[1] * basins[2]
+
+
+def get_low_points(data):
     low_points = []
     for i, line in enumerate(data):
         for j, c in enumerate(line):
@@ -27,50 +68,8 @@ def part_one(data):
                 neighbors.append(data[i][j + 1])
 
             if all([int(n) > num for n in neighbors]):
-                low_points.append(num)
-
-    return sum(low_points) + len(low_points)
-
-
-def part_two(data):
-    taken = [[False] * len(data[0]) for _ in range(len(data))]
-    neighbors = []
-    basins = []
-    basin = -1
-    while not all([all(line) for line in taken]):
-        if len(neighbors) == 0:
-            i, j = 0, 0
-            while taken[i][j]:
-                i += 1
-                if i == len(data):
-                    i = 0
-                    j += 1
-            basin += 1
-            basins.append(0)
-        else:
-            i, j = neighbors.pop()
-
-        if taken[i][j]:
-            continue
-
-        taken[i][j] = True
-
-        if data[i][j] == "9":
-            continue
-
-        basins[basin] += 1
-
-        if i != 0 and not taken[i - 1][j]:
-            neighbors.append((i-1, j))
-        if i != len(data) - 1 and not taken[i + 1][j]:
-            neighbors.append((i+1, j))
-        if j != 0 and not taken[i][j - 1]:
-            neighbors.append((i, j-1))
-        if j != len(data[i]) - 1 and not taken[i][j + 1]:
-            neighbors.append((i, j+1))
-
-    basins.sort(reverse=True)
-    return basins[0] * basins[1] * basins[2]
+                low_points.append((i, j))
+    return low_points
 
 
 if __name__ == "__main__":
