2bb516a7e5b7be15120a0acb17a8951e0b87014f
Duuk Niemantsverdriet <D.P.Niemantsverdriet@student.tudelft.nl>
2021-12-12 07:01:29 +0100

    Shorten day 1

--- days/day01.py
+++ days/day01.py
@@ -10,11 +10,11 @@ def solve(data, part=2):
 
 
 def part_one(data):
-    return sum([1 if data[i] > data[i - 1] else 0 for i in range(1, len(data))])
+    return sum([data[i] > data[i - 1] for i in range(1, len(data))])
 
 
 def part_two(data):
-    return sum([1 if data[i] > data[i - 3] else 0 for i in range(3, len(data))])
+    return sum([data[i] > data[i - 3] for i in range(3, len(data))])
 
 
 if __name__ == "__main__":
